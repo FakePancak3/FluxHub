72660e772e48bd8d67221887bfaa21d017791416
PixelatedColors <95421998+PixelatedColors@users.noreply.github.com>
2026-01-03 14:38:41 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,61 +1,11 @@
-print("/ / / / / / / / HYDROX REFRESHED / / / / / / / /")
-print("Thank you for using Hydrox REFRESHED")
+print("/ / / / / / / / NEXUS V1 / / / / / / / /")
+print("Thank you for using Nexus")
 wait(1)
-print("VERSION CHECKER LOADING....")
+print("[DEBUG] VERSION CHECKER LOADING....")
 wait(1)
-print("VERSION - K5/1/26 (LATEST)")
+print("[DEBUG] VERSION - N5/1/26 (LATEST)")
 wait(1)
-print("Installing Dependencies (takes longer on different executors)")
+print("[DEBUG] Installing Dependencies")
 wait(2)
-print("Loading UILibrary")
-print("Loaded!")
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "🎆 Hydrox - REFRESHED (K5/1/26)",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Loading...",
-   LoadingSubtitle = "hydrox.main",
-   ShowText = "Hydrox", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "hydrox"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Hydrox REFRESHED | Key System",
-      Subtitle = "Key System",
-      Note = "Open the link in your browser.", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/Pth0rxbW"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
-local Tab = Window:CreateTab("Tab Example", "house")
-local Section = Tab:CreateSection("Information")
-local Input = Tab:CreateInput({
-   Name = "Input Example",
-   CurrentValue = "",
-   PlaceholderText = "Input Placeholder",
-   RemoveTextAfterFocusLost = false,
-   Flag = "Input1",
-   Callback = function(Text)
-   -- The function that takes place when the input is changed
-   -- The variable (Text) is a string for the value in the text box
-   end,
-})
+print("[DEBUG] Loading UILibrary")
+print("[DEBUG] CRITICAL ERROR, ABORTED.")
